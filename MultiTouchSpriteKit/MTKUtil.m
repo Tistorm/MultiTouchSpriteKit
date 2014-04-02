5c70c6caf195cdef8d4586d72b0dc624b6afbe81
Simon Voelker <voelker@cs.rwth-aachen.de>
2014-04-02 09:18:43 +0200

    added CGcontext function into MTKUtil. Preparing for shape and gradient using CALayers

--- MultiTouchSpriteKit/MTKUtil.m
+++ MultiTouchSpriteKit/MTKUtil.m
@@ -9,3 +9,52 @@
 #import "MTKUtil.h"
 
 
+/**
+ *  Creates a CGContextRef for a Image
+ *  This method was taken from http://stackoverflow.com/questions/19490970/sprite-kit-and-colorwithpatternimage (Johnny)
+ 
+ *
+ *  @param pixelsWide Width of the context.
+ *  @param pixelsHigh Height of the context.
+ *
+ *  @return The BitmapContext
+ */
+// ------------------------------------------------------
+CGContextRef createBitmapContext(const size_t pixelsWide, const size_t pixelsHigh)
+// ------------------------------------------------------
+{
+    CGContextRef    context = NULL;
+    CGColorSpaceRef colorSpace;
+    void *          bitmapData;
+    //int             bitmapByteCount;
+    size_t             bitmapBytesPerRow;
+    
+    bitmapBytesPerRow   = (pixelsWide * 4);// 1
+    //bitmapByteCount     = (bitmapBytesPerRow * pixelsHigh);
+    
+    colorSpace = CGColorSpaceCreateDeviceRGB();
+    bitmapData = NULL;
+    
+#define kBitmapInfo     kCGImageAlphaPremultipliedLast
+    //#define kBitmapInfo       kCGImageAlphaPremultipliedFirst
+    //#define kBitmapInfo       kCGImageAlphaNoneSkipFirst
+    // According to http://stackoverflow.com/a/18921840/129202 it should be safe to just cast
+    CGBitmapInfo bitmapinfo = (CGBitmapInfo)kBitmapInfo; //kCGImageAlphaNoneSkipFirst; //0; //kCGBitmapAlphaInfoMask; //kCGImageAlphaNone; //kCGImageAlphaNoneSkipFirst;
+    context = CGBitmapContextCreate (bitmapData,// 4
+                                     pixelsWide,
+                                     pixelsHigh,
+                                     8,      // bits per component
+                                     bitmapBytesPerRow,
+                                     colorSpace,
+                                     bitmapinfo
+                                     );
+    if (context== NULL)
+    {
+        free (bitmapData);// 5
+        fprintf (stderr, "Context not created!");
+        return NULL;
+    }
+    CGColorSpaceRelease( colorSpace );// 6
+    
+    return context;// 7
+}
